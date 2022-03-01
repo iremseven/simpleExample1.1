a167edd39be69df874edd3d433fc2ce8a7d56746
Hp <iremirem201920@gmail.com>
2022-03-01 13:44:04 +0300

    more lines-d added

--- src/tredu/maltepe/oop/Example1.java
+++ src/tredu/maltepe/oop/Example1.java
@@ -5,6 +5,7 @@ public class Example1 {
     public static void main(String[] args) {
 
     System.out.println("merhaba");
-
+    System.out.println("merhaba");
+    System.out.println("merhaba");
     }
 }
